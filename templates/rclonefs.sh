a70ff0cceef938cef5a3b0dcbd487e3e131239bc
Ivan Andreev <ivandeex@gmail.com>
2020-10-21 19:46:45 +0300

    several rclonefs improvements
    - fix check that hung for empty mounts
    - improve value extraction
    - implement background log

--- templates/rclonefs.sh
+++ templates/rclonefs.sh
@@ -2,10 +2,11 @@
 #set -x
 
 remote="$1"
-mountpoint="$2"
+mountpoint="${2%%/}"
 shift 2
 
 wait=yes
+bglog=no
 foreground=no
 rclone="{{ lin_rclone_binary }}"
 args=""
@@ -32,16 +33,18 @@ while getopts :o: opts; do
         continue ;;
       # wrapper options
       proxy=*)
-        export http_proxy=${param#proxy=}
-        export https_proxy=${param#proxy=} ;;
+        export http_proxy=${param#*=}
+        export https_proxy=${param#*=} ;;
       config=*)
-        export RCLONE_CONFIG=${param#config=} ;;
+        export RCLONE_CONFIG=${param#*=} ;;
       verbose=*)
-        export RCLONE_VERBOSE=${param#verbose=} ;;
+        export RCLONE_VERBOSE=${param#*=} ;;
       nowait)
         wait=no ;;
       foreground)
         foreground=yes ;;
+      bglog)
+        bglog=yes ;;
       # fuse / rclone options
       allow_other|allow_root|uid=*|gid=*)
         args="$args --${param//_/-}" ;;
@@ -52,6 +55,21 @@ while getopts :o: opts; do
   done
 done
 
+if [ $bglog = yes ]; then
+  stamp=$(date '+%y%m%d-%H%M%S')
+  pid=$$
+  where=$(basename "$mountpoint")
+  logfile=/tmp/rclone-${stamp}-${pid}-${where}.log
+  touch "$logfile"
+  chmod 666 "$logfile"
+  # activate verbose background logging
+  export RCLONE_VERBOSE=3
+  export RCLONE_LOG_FORMAT=date,time,microseconds
+  export RCLONE_LOG_FILE=$logfile
+  # deactivate systemd log flavor in rclone
+  unset INVOCATION_ID
+fi
+
 # exec rclone (shellcheck note: args must stay unquoted)
 if [ $foreground = yes ]; then
   # shellcheck disable=SC2086
@@ -60,8 +78,7 @@ else
   # NOTE: --daemon hangs under systemd automount, using `&`
   # shellcheck disable=SC2086
   "$rclone" mount $args "$remote" "$mountpoint" </dev/null >&/dev/null &
-  # WARNING: this check hangs for empty mounts!
-  while [ $wait = yes ] && [ "$(ls -lA "$mountpoint")" = 'total 0' ]; do
+  while [ $wait = yes ] && [ "$(grep -c " ${mountpoint} fuse.rclone " /proc/mounts)" = 0 ]; do
     sleep 0.5
   done
 fi
